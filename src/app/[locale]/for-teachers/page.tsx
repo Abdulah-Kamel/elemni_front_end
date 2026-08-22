c0960f945abf6f27bcc28ac54e1029527bc1ecc3
Abdullah_Kamel <abdullahkamel.750@gmail.com>
2026-08-22 20:48:25 +0300

    refactor: move for-teachers ownership into marketing

--- src/app/[locale]/for-teachers/page.tsx
+++ src/app/[locale]/for-teachers/page.tsx
@@ -1,6 +1,6 @@
-import TeacherMarketingPage from "@/src/features/teacher-marketing/page";
+import TeacherMarketingPage from "@/src/features/marketing/page";
 
-export { generateMetadata } from "@/src/features/teacher-marketing/page";
+export { generateMetadata } from "@/src/features/marketing/page";
 
 export default async function ForTeachersPage({
   params,
